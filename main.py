fd72d3f40e02f76f712e6a26cd6eaa4856ddf171
Ab <77987005+TrvstxdAb@users.noreply.github.com>
2021-07-24 22:36:50 +0300

    new linlk

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ init(convert=True)
 print('%sTry not to skid this :) how many codes?%s ' % (Fore.CYAN, Fore.WHITE), end='')
 amount = int(input())
 for i in range(amount):
-    code = "https://discordapp.com/gifts/%s" % (('').join(random.choices(string.ascii_letters + string.digits, k=16)))
+    code = "https://discord.gift/%s" % (('').join(random.choices(string.ascii_letters + string.digits, k=16)))
     print('Code: %s' % (code))
     with open('codes.txt', 'a') as f:
         f.write('%s\n' % (code))
